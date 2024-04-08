c00728b4cb59dd53f05116add2f8a6209b971900
“Jonah <etuafuljonah@gmail.com>
2024-04-08 19:08:53 +0000

    third commit

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,8 @@
 export default function getNeighborhoodsList() {
-  	var sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-	const addNeighborhood = (newNeighborhood) => {
-    		sanFranciscoNeighborhoods.push(newNeighborhood);
-		return sanFranciscoNeighborhoods;
-	};
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
 }
